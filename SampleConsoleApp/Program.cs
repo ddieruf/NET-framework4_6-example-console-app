a7bad8eb15c9e39f2e1848a9c8c1777b0bad0757
David Dieruf <david@dierufventures.com>
2018-03-09 21:53:11 -0500

    Trying to make it work

--- SampleConsoleApp/Program.cs
+++ SampleConsoleApp/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace SampleConsoleApp
 {
@@ -15,6 +15,11 @@ namespace SampleConsoleApp
 
 			_logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 			_logger.Info("Hi there again!");
+
+			for (int i = 0; i < 100; i++) {
+				_logger.Info(String.Format("The number {0}", i));
+				Thread.Sleep(10000);
+			}
 		}
 	}
 }
